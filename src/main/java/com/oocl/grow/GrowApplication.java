f93be33429e28562707ab82c804d6be62f77ac3e
Seaborn Lee <seabornlee.cn@gmail.com>
2019-07-04 11:42:39 +0800

    chore: init

---
+++ src/main/java/com/oocl/grow/GrowApplication.java
@@ -0,0 +1,13 @@
+package com.oocl.grow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrowApplication.class, args);
+	}
+
+}
